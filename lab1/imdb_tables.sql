bc8cd5390159dad79a4936f0a23427f6a0bcfb7a
Blake Schmidt <blakeschmidt@wireless-10-147-219-157.public.utexas.edu>
2017-09-20 19:10:14 -0500

    add rest of tables

--- lab1/imdb_tables.sql
+++ lab1/imdb_tables.sql
@@ -54,3 +54,66 @@ WITH SERDEPROPERTIES (
   'field.delim' = ','
 ) LOCATION 's3://cs327e-fall2017-imdb/athena/stars/'
 TBLPROPERTIES ('has_encrypted_data'='false');
+
+CREATE EXTERNAL TABLE IF NOT EXISTS imdb.title_basics (
+  `title_id` string,
+  `title_type` string,
+  `primary_title` string,
+  `is_adult` boolean,
+  `start_year` int,
+  `end_year` int,
+  `runtime_minutes` int 
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe'
+WITH SERDEPROPERTIES (
+  'serialization.format' = ',',
+  'field.delim' = ','
+) LOCATION 's3://cs327e-fall2017-imdb/athena/title_basics/'
+TBLPROPERTIES ('has_encrypted_data'='false');
+
+CREATE EXTERNAL TABLE IF NOT EXISTS imdb.title_episodes (
+  `title_id` string,
+  `parent_title` string,
+  `season_num` int,
+  `episode_num` int 
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe'
+WITH SERDEPROPERTIES (
+  'serialization.format' = ',',
+  'field.delim' = ','
+) LOCATION 's3://cs327e-fall2017-imdb/athena/title_episodes/'
+TBLPROPERTIES ('has_encrypted_data'='false');
+
+CREATE EXTERNAL TABLE IF NOT EXISTS imdb.title_genres (
+  `title_id` string,
+  `genre` string 
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe'
+WITH SERDEPROPERTIES (
+  'serialization.format' = ',',
+  'field.delim' = ','
+) LOCATION 's3://cs327e-fall2017-imdb/athena/title_genres/'
+TBLPROPERTIES ('has_encrypted_data'='false');
+
+CREATE EXTERNAL TABLE IF NOT EXISTS imdb.title_ratings (
+  `title_id` string,
+  `average_rating` double,
+  `num_votes` int 
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe'
+WITH SERDEPROPERTIES (
+  'serialization.format' = ',',
+  'field.delim' = ','
+) LOCATION 's3://cs327e-fall2017-imdb/athena/title_ratings/'
+TBLPROPERTIES ('has_encrypted_data'='false');
+
+CREATE EXTERNAL TABLE IF NOT EXISTS imdb.writers (
+  `title_id` string,
+  `person_id` string 
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe'
+WITH SERDEPROPERTIES (
+  'serialization.format' = ',',
+  'field.delim' = ','
+) LOCATION 's3://cs327e-fall2017-imdb/athena/writers/'
+TBLPROPERTIES ('has_encrypted_data'='false');
