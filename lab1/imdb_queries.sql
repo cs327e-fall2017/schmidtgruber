90070cfae578e93a2c10aaf92aba376b9fba096d
Kyle Chance Gruber <kylegruber@Kyles-MacBook-Pro.local>
2017-09-22 21:12:52 -0500

    added Queries 11-20

--- lab1/imdb_queries.sql
+++ lab1/imdb_queries.sql
@@ -75,7 +75,81 @@ where tb.title_type = 'movie'
 and tg.genre = 'Comedy'
 order by tr.average_rating desc;
 
+/* Query 11: Find all movies/shows with Zach Galifianakis sorted by start year, from newest to oldest */
+select tb.title_id, tb.primary_title, tb.start_year
+from title_basics tb join principals p on tb.title_id = p.title_id
+join person_basics pb on pb.person_id = p.person_id
+where pb.primary_name = 'Zach Galifianakis'
+and tb.start_year is not null
+order by tb.start_year desc;
 
+/* Query 12: Find all TV series with more than 10 seasons sorted by end year, from lowest to highest */
+select distinct tb.title_id, tb.primary_title, tb.end_year,
+from title_basics tb join title_episodes te on tb.title_id = te.parent_title
+where te.season_num = 11
+and tb.title_type = 'tvSeries'
+and tb.start_year is not null
+order by tb.start_year;
 
+/* Query 13: Find all TV episodes with David Blaine sorted by rating, from lowest to highest*/
+select tb.title_id, tb.primary_title, tr.average_rating
+from title_basics tb join stars s on tb.title_id = s.title_id
+join person_basics pb on pb.person_id = s.person_id
+join title_ratings tr on tr.title_id = tb.title_id
+where pb.primary_name = 'David Blaine'
+and tb.title_type = 'tvEpisode'
+and tr.average_rating is not null
+order by tb.average_rating;
 
+/* Query 14: Find all movies and genres with an average rating of 8 or higher sorted by start year, from newest to oldest */
+select tb.primary_title, tg.genre, tr.average_rating, 
+from title_basics tb join title_genres tg on tb.title_id = tg.title_id
+join title_ratings tr on tb.title_id = tr.title_id
+where tb.title_type = 'movie'
+and tr.average_rating >= 8
+order by tb.start_year desc;
 
+/* Query 15: Find all Thriller movies that are adult, sorted by length from shortest to longest*/
+select tb.primary_title, tb.runtime_minutes
+from title_basics tb join title_genres tg on tb.title_id = tg.title_id
+where tb.title_type = 'movie'
+and tg.genre = 'Thriller'
+and tb.is_adult = TRUE
+order by tb.runtime_minutes;
+
+/* Query 16: Find all movies from 1995 */
+select tb.primary_title, tb.start_year
+from title_basics tb
+where tb.title_type = 'movie'
+and tb.start_year = 1995;
+
+/* Query 17: Find all drama TV series and sort by number of votes from highest to lowest*/
+select tb.primary_title, tb.start_year, tr.num_votes
+from title_genres tg join title_basics tb on tb.title_id = tg.title_id
+join title_ratings tr on tr.title_id = tb.title_id
+where tb.title_type = 'tvSeries'
+and tg.genre = 'Drama'
+order by tr.num_votes desc;
+
+/* Query 18: Find all writers dead after 1945 and before 1965 sorted by death year from oldest to most recent*/
+select distinct pb.primary_name, pb.death_year
+from writers w join person_basics pb on w.person_id = pb.person_id
+where pb.death_year is not null
+and pb.death_year >= 1945
+and pb.death_year <= 1965
+order by pb.death_year;
+
+/* Query 19: Find movies longer than 3 hours, limited to 1000 movies*/
+select tb.primary_title, tb.runtime_minutes
+from title_basics tb
+where tb.title_type = 'movie'
+and tb.runtime_minutes > 180
+limit 1000;
+
+/* Query 20: Find movies with a runtime between 1 hr and 1.5 hrs with an average rating of atleast 9.5*/
+select tb.primary_title, tb.runtime_minutes, tr.average_rating
+from title_basics tb join title_ratings tr on tb.title_id = tr.title_id
+where tb.title_type = 'movie'
+and tb.runtime_minutes >= 60
+and tb.runtime_minutes <= 90
+and tr.average_rating >= 9.5;
