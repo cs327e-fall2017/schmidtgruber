c4ed9249a50360ae23c60d0d4b71e06ec5d4e50a
Blake Schmidt <blakecschmidt@gmail.com>
2017-10-04 19:24:23 -0500

    fixed syntax errors

--- lab2/create_base_tables.sql
+++ lab2/create_base_tables.sql
@@ -1,101 +1,76 @@
-CREATE TABLE title_basics
-	(title_id char (9) PRIMARY KEY,
-	title_type varchar (20),
-	primary_title varchar (300),
-	original_title varchar (300),
+create table title_basics(
+	title_id char(9) primary key,
+	title_type varchar(20),
+	primary_title varchar(300),
+	original_title varchar(300),
 	is_adult boolean,
 	start_year int,
 	end_year int,
-	runtime_minutes int);
+	runtime_minutes int
+);
 
-CREATE TABLE person_basics
-	(person_id char (9) PRIMARY KEY,
-	primary_name varchar (110),
+create table person_basics(
+	person_id char(9) primary key,
+	primary_name varchar(110),
 	birth_year int,
-	death_year int);
+	death_year int
+);
 
-CREATE TABLE title_ratings
-	(title_id char (9) PRIMARY KEY,
+create table title_ratings(
+	title_id char(9) primary key,
 	average_rating numeric(3,1),
-	num_votes int
-	-- ERROR:  syntax error at or near "FOREIGN"
-	FOREIGN KEY title_ratings2title_basics (title_id)
-	REFERENCES title_basics
-		ON UPDATE CASCADE
-		ON DELETE CASCADE);
+	num_votes int,
+	foreign key(title_id) references title_basics
+);
 
-CREATE TABLE principals
-	(title_id char (9),
-	person_id char (9)
-	-- ERROR:  syntax error at or near "("
-	PRIMARY KEY (title_id, person_id)
-	FOREIGN KEY principals2title_basics (title_id)
-	REFERENCES title_basics
-		ON UPDATE CASCADE
-		ON DELETE CASCADE);
+create table principals(
+	title_id char(9),
+	person_id char(9),
+	primary key(title_id, person_id),
+	foreign key(title_id) references title_basics
+);
 
-CREATE TABLE stars
-	(person_id char (9),
-	title_id char (9)
-	-- ERROR:  syntax error at or near "("
-	PRIMARY KEY (person_id, title_id)
-	FOREIGN KEY stars2person_basics (person_id)
-	REFERENCES person_basics
-		ON UPDATE CASCADE
-		ON DELETE CASCADE
-	FOREIGN KEY stars2title_basics (title_id)
-	REFERENCES title_basics
-		ON UPDATE CASCADE
-		ON DELETE CASCADE);
+create table stars(
+	person_id char(9),
+	title_id char(9),
+	primary key(person_id, title_id),
+	foreign key(person_id) references person_basics,
+	foreign key(title_id) references title_basics
+);
 
-CREATE TABLE directors
-	(title_id char (9),
-	person_id char (9),
-	PRIMARY KEY (title_id, person_id)
-	-- ERROR:  syntax error at or near "FOREIGN"
-	FOREIGN KEY directors2title_basics (title_id)
-	REFERENCES title_basics
-		ON UPDATE CASCADE
-		ON DELETE CASCADE
-	FOREIGN KEY directors2person_basics (person_id)
-	REFERENCES person_basics
-		ON UPDATE CASCADE
-		ON DELETE CASCADE);
+create table directors(
+	title_id char(9),
+	person_id char(9),
+	primary key(title_id, person_id),
+	foreign key(title_id) references title_basics,
+	foreign key(person_id) references person_basics
+);
 
-CREATE TABLE person_professions
-	(person_id char (9),
-	profession varchar (30),
-	PRIMARY KEY (person_id, profession)
-	-- ERROR:  syntax error at or near "FOREIGN"
-	FOREIGN KEY person_professions2person_basics (person_id)
-	REFERENCES person_basics
-		ON UPDATE CASCADE
-		ON DELETE CASCADE);
+create table person_professions(
+	person_id char(9),
+	profession varchar(30),
+	primary key(person_id, profession),
+	foreign key(person_id) references person_basics
+);
 
-CREATE TABLE title_episodes
-	(title_id char (9) PRIMARY KEY,
-	parent_title char (9),
+create table title_episodes(
+	title_id char(9) primary key,
+	parent_title char(9),
 	season_num int,
 	episode_num int,
-	-- ERROR:  syntax error at or near "title_episode2title_basics"
-	FOREIGN KEY title_episode2title_basics (title_id)
-	REFERENCES title_basics
-		ON UPDATE CASCADE
-		ON DELETE CASCADE);
+	foreign key(title_id) references title_basics
+);
 
-CREATE TABLE title_genres
-	(title_id char (9),
-	genre varchar (20),
-	PRIMARY KEY (title_id, genre));
+create table title_genres(
+	title_id char(9),
+	genre varchar(20),
+	primary key(title_id, genre)
+);
 
-CREATE TABLE writers
-	(title_id char (9),
-	person_id varchar (20),
-	PRIMARY KEY (title_id, person_id)
-	-- ERROR:  syntax error at or near "FOREIGN"
-	FOREIGN KEY writers2title_basics (title_id)
-		ON UPDATE CASCADE
-		ON DELETE CASCADE
-	FOREIGN KEY writers2person_basics (person_id)
-		ON UPDATE CASCADE
-		ON DELETE CASCADE);
+create table writers(
+	title_id char(9),
+	person_id varchar(20),
+	primary key(title_id, person_id),
+	foreign key(title_id) references title_basics,
+	foreign key(person_id) references person_basics
+);
