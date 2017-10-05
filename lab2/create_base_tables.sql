ba77e3b650b3ac98b5176a89bc12ed77c74086e5
Blake Schmidt <blakecschmidt@gmail.com>
2017-10-05 17:10:09 -0500

    updated sql file

--- lab2/create_base_tables.sql
+++ lab2/create_base_tables.sql
@@ -1,5 +1,5 @@
 create table title_basics(
-	title_id char(9) primary key,
+	title_id varchar(10) primary key,
 	title_type varchar(20),
 	primary_title varchar(300),
 	original_title varchar(300),
@@ -10,65 +10,65 @@ create table title_basics(
 );
 
 create table person_basics(
-	person_id char(9) primary key,
+	person_id varchar(10) primary key,
 	primary_name varchar(110),
 	birth_year int,
 	death_year int
 );
 
 create table title_ratings(
-	title_id char(9) primary key,
+	title_id varchar(10) primary key,
 	average_rating numeric(3,1),
 	num_votes int,
 	foreign key(title_id) references title_basics
 );
 
 create table principals(
-	title_id char(9),
-	person_id char(9),
+	title_id varchar(10),
+	person_id varchar(10),
 	primary key(title_id, person_id),
 	foreign key(title_id) references title_basics
 );
 
 create table stars(
-	person_id char(9),
-	title_id char(9),
+	person_id varchar(10),
+	title_id varchar(10),
 	primary key(person_id, title_id),
 	foreign key(person_id) references person_basics,
 	foreign key(title_id) references title_basics
 );
 
 create table directors(
-	title_id char(9),
-	person_id char(9),
+	title_id varchar(10),
+	person_id varchar(10),
 	primary key(title_id, person_id),
 	foreign key(title_id) references title_basics,
 	foreign key(person_id) references person_basics
 );
 
 create table person_professions(
-	person_id char(9),
+	person_id varchar(10),
 	profession varchar(30),
 	primary key(person_id, profession),
 	foreign key(person_id) references person_basics
 );
 
 create table title_episodes(
-	title_id char(9) primary key,
-	parent_title char(9),
+	title_id varchar(10) primary key,
+	parent_title varchar(10),
 	season_num int,
 	episode_num int,
 	foreign key(title_id) references title_basics
 );
 
 create table title_genres(
-	title_id char(9),
+	title_id varchar(10),
 	genre varchar(20),
 	primary key(title_id, genre)
 );
 
 create table writers(
-	title_id char(9),
+	title_id varchar(10),
 	person_id varchar(20),
 	primary key(title_id, person_id),
 	foreign key(title_id) references title_basics,
