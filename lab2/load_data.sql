bf3e694f08145a0aeb689bd2db088755821e3895
Kyle Chance Gruber <kylegruber@skrrrt.local>
2017-10-06 01:04:37 -0500

    added star schema

--- lab2/load_data.sql
+++ lab2/load_data.sql
@@ -1,19 +1,19 @@
-\copy title_basics from /Users/blakeschmidt/Downloads/pg/title_basics.csv (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
+\copy title_basics from /Users/kylegruber/Downloads/pg/title_basics.csv (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
 
-\copy person_basics from /Users/blakeschmidt/Downloads/pg/person_basics.csv (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
+\copy person_basics from /Users/kylegruber/Downloads/pg/person_basics.csv (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
 
-\copy title_ratings from /Users/blakeschmidt/Downloads/pg/title_ratings.csv (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
+\copy title_ratings from /Users/kylegruber/Downloads/pg/title_ratings.csv (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
 
-\copy principals from /Users/blakeschmidt/Downloads/pg/principals.csv (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
+\copy principals from /Users/kylegruber/Downloads/pg/principals.csv (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
 
-\copy stars from /Users/blakeschmidt/Downloads/pg/stars.csv (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
+\copy stars from /Users/kylegruber/Downloads/pg/stars.csv (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
 
-\copy directors from /Users/blakeschmidt/Downloads/pg/directors.csv (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
+\copy directors from /Users/kylegruber/Downloads/pg/directors.csv (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
 
-\copy person_professions from /Users/blakeschmidt/Downloads/pg/person_professions.csv (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
+\copy person_professions from /Users/kylegruber/Downloads/pg/person_professions.csv (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
 
-\copy title_episodes from /Users/blakeschmidt/Downloads/pg/title_episodes.csv (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
+\copy title_episodes from /Users/kylegruber/Downloads/pg/title_episodes.csv (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
 
-\copy title_genres from /Users/blakeschmidt/Downloads/pg/title_genres.csv (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
+\copy title_genres from /Users/kylegruber/Downloads/pg/title_genres.csv (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
 
-\copy title_writers from /Users/blakeschmidt/Downloads/pg/writers.csv (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
+\copy title_writers from /Users/kylegruber/Downloads/pg/writers.csv (header TRUE, format csv, delimiter ',', null '', encoding 'UTF8');
