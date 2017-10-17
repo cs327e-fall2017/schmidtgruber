8b1536fd672db0ca6a35ec1a2c6c45610665a58d
Blake Schmidt <blakecschmidt@gmail.com>
2017-10-17 16:42:08 -0500

    fixed errors

--- lab3/imdb_aggr_queries.sql
+++ lab3/imdb_aggr_queries.sql
@@ -1,5 +1,5 @@
 /* 2 inner joins, 1 where clause, 1 having clause
-/* Actors who have acted in at least 1 titles per year between the years 1995 - 2000 */
+/* Query 1: Actors who have acted in at least 1 titles per year between the years 1995 - 2000 */
 select primary_name as star_name, start_year as year, count(*) as title_count
 from title_basics tb join stars s on tb.title_id = s.title_id
 join person_basics pb on pb.person_id = s.person_id
@@ -8,11 +8,12 @@ group by primary_name, start_year
 having count(*) >= 1
 order by start_year, count(*);
 
-/* 2 inner joins, 1 where clause, 1 having clause
+/* 3 inner joins, 1 where clause, 1 having clause
 /* Query 2: Writers who have written at least 2 Drama titles per year since the year 2000 */
 select primary_name as writer_name, start_year as year, count(*) as title_count
 from title_basics tb join writers w on tb.title_id = w.title_id
 join person_basics pb on pb.person_id = w.person_id
+join title_genres tg on tg.title_id = tb.title_id
 where start_year between 2000 and 2017
 and genre = "Drama"
 group by primary_name, start_year
