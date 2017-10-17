e8be163ef42437285ba7da201190c6658f3e5237
Blake Schmidt <blakecschmidt@gmail.com>
2017-10-17 16:22:47 -0500

    added a query

--- lab3/imdb_aggr_queries.sql
+++ lab3/imdb_aggr_queries.sql
@@ -14,6 +14,7 @@ select primary_name as writer_name, start_year as year, count(*) as title_count
 from title_basics tb join writers w on tb.title_id = w.title_id
 join person_basics pb on pb.person_id = w.person_id
 where start_year between 2000 and 2017
+and genre = "Drama"
 group by primary_name, start_year
 having count(*) >= 2
 order by start_year, count(*);
@@ -26,6 +27,16 @@ join person_basics pb on pb.person_id = w.person_id
 where start_year between 2000 and 2017
 group by primary_name, start_year
 order by start_year, count(*);
+
+/* 2 inner joins, 1 where clause, 1 having clause
+/* Query 4: TV Shows that have at least 100 episodes and a rating of 5 or higher */
+select primary_title as title_name, average_rating as rating, start_year as year, count(*) as episode_count
+from title_basics tb join title_episodes te on tb.title_id = te.title_id
+join title_ratings tr on tb.title_id = tr.title_id
+where average_rating >= 5.0
+group by title_name, episodes
+having count(*) >= 100
+order by episodes, count(*);
 
 /*  Inner Join Count: 4
 	Outer Join Count: 1
