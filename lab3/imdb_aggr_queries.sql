7e60b1e9a092c40665406e81289fdbb89c49c5d1
Kyle Chance Gruber <kylegruber@wireless-10-147-127-82.public.utexas.edu>
2017-10-17 16:31:48 -0500

    updated aggr queries

--- lab3/imdb_aggr_queries.sql
+++ lab3/imdb_aggr_queries.sql
@@ -19,14 +19,15 @@ group by primary_name, start_year
 having count(*) >= 2
 order by start_year, count(*);
 
-/* 1 outer joins, 1 where clause, 1 having clause
-/* Query 3: Writers who have written titles since the year 2000 */
-select primary_name as writer_name, start_year as year, count(*) as title_count
-from title_basics tb join writers w on tb.title_id = w.title_id
-join person_basics pb on pb.person_id = w.person_id
-where start_year between 2000 and 2017
-group by primary_name, start_year
-order by start_year, count(*);
+/* 3 inner joins, 1 where clause
+/* Query 3: Directors who have directed titles with average ratings between 8 and 10 */
+select primary_name as director_name, average_rating as rating, count(*) as title_count
+from title_basics tb join directors d on tb.title_id = d.title_id
+join person_basics pb on pb.person_id = d.person_id
+join title_ratings tr on tr.title_id = d.title_id
+where rating between 8.0 and 10.0
+group by primary_name, average_rating
+order by average_rating, count(*);
 
 /* 2 inner joins, 1 where clause, 1 having clause
 /* Query 4: TV Shows that have at least 100 episodes and a rating of 5 or higher */
@@ -38,7 +39,8 @@ group by title_name, episodes
 having count(*) >= 100
 order by episodes, count(*);
 
-/*  Inner Join Count: 4
-	Outer Join Count: 1
-	Where Clause Count: 3
-	Having Clause Count: 3
+/*  Inner Join Count: DONE
+	Outer Join Count: 0/2
+	Where Clause Count: DONE
+	Having Clause Count: DONE
+	Bast tables still needing to be accessed: principals, person_professions, title_episodes, 
