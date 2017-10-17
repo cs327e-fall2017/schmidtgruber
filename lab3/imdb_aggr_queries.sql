b19320e0bc1e23a1ccea9082daae8def8531e28a
Blake Schmidt <blakecschmidt@gmail.com>
2017-10-17 16:05:35 -0500

    added a query

--- lab3/imdb_aggr_queries.sql
+++ lab3/imdb_aggr_queries.sql
@@ -7,3 +7,13 @@ where start_year between 1995 and 2000
 group by primary_name, start_year
 having count(*) >= 1
 order by start_year, count(*);
+
+/* 2 inner joins, 1 where clause, 1 having clause
+/* Query 2: Writers who have written at least 2 Drama movies per year since the year 2000 */
+select primary_name as writer_name, start_year as year, count(*) as title_count
+from title_basics tb join writers w on tb.title_id = w.title_id
+join person_basics pb on pb.person_id = w.person_id
+where start_year between 2000 and 2017
+group by primary_name, start_year
+having count(*) >= 2
+order by start_year, count(*);
