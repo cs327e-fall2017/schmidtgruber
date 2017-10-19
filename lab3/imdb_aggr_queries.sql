d280a3e3948293f8b3e5f732ad9a0f43c0abab99
Blake Schmidt <blakecschmidt@gmail.com>
2017-10-18 19:33:31 -0500

    finished up queries

--- lab3/imdb_aggr_queries.sql
+++ lab3/imdb_aggr_queries.sql
@@ -1,4 +1,3 @@
-/* 2 inner joins, 1 where clause, 1 having clause
 /* Query 1: Actors who have acted in at least 6 titles in a year between the years 1995 - 2000 */
 select primary_name as star_name, start_year as year, count(*) as title_count
 from title_basics tb join stars s on tb.title_id = s.title_id
@@ -8,7 +7,6 @@ group by primary_name, start_year
 having count(*) >= 6
 order by primary_name, start_year, count(*);
 
-/* 3 inner joins, 1 where clause, 1 having clause
 /* Query 2: Writers who have written at least 2 Drama titles per year since the year 2000 */
 select primary_name as writer_name, start_year as year, count(*) as title_count
 from title_basics tb join writers w on tb.title_id = w.title_id
@@ -20,7 +18,6 @@ group by primary_name, start_year
 having count(*) >= 2
 order by primary_name, start_year, count(*);
 
-/* 3 inner joins, 1 where clause
 /* Query 3: Directors who have directed titles with average ratings between 8 and 10 */
 select primary_name as director_name, average_rating as rating, count(*) as title_count
 from title_basics tb join directors d on tb.title_id = d.title_id
@@ -30,7 +27,6 @@ where average_rating between 8.0 and 10.0
 group by primary_name, average_rating
 order by primary_name, average_rating, count(*);
 
-/* 2 inner joins, 1 where clause, 1 having clause
 /* Query 4: TV Shows that have at least 100 episodes */
 select primary_title as title, count(*) as episode_count
 from title_episodes te join title_basics tb on te.parent_title_id = tb.title_id
@@ -47,19 +43,12 @@ group by primary_name
 having count(*) >= 5
 order by count(*) desc;
 
- /* Query 6: Actors who are in at least 50 titles that they arent a principal actor in */
+ /* Query 6: Actors who are in at least 25 titles that they arent a principal actor in */
 select primary_name as name, count(*) as not_principal
 from person_professions pp left outer join principals p on pp.person_id = p.person_id
 join person_basics pb on pb.person_id = pp.person_id
 where profession = 'actor'
 and p.title_id is null
 group by primary_name
-having count(*) > 50
+having count(*) > 25
 order by count(*) desc;
-
-/*  Inner Join Count: DONE
-	Outer Join Count: 0/2
-	Where Clause Count: DONE
-	Having Clause Count: DONE
-	Base tables still needing to be accessed: principals, person_professions, 
-*/
