e4cf21987eeab595c530138547bb587e922c6aa3
Blake Schmidt <blakecschmidt@gmail.com>
2017-10-18 19:24:24 -0500

    edited query

--- lab3/imdb_aggr_queries.sql
+++ lab3/imdb_aggr_queries.sql
@@ -45,13 +45,14 @@ group by primary_name
 having count(person_id) >= 5
 order by count(*);
 
- /* Query 6: How many times an actor is in a title and isnt a principal actor */
+ /* Query 6: Actors who are in at least 50 titles that they arent a principal actor in */
 select primary_name as name, count(*) as not_principal
 from person_professions pp left outer join principals p on pp.person_id = p.person_id
 join person_basics pb on pb.person_id = pp.person_id
 where profession = 'actor'
 and p.title_id is null
 group by primary_name
+having count(*) > 50
 order by count(*) desc;
 
 /*  Inner Join Count: DONE
