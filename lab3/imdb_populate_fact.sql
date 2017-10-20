73e5bb97ca4790d051bf2780df134b06c726dee8
Blake Schmidt <blakecschmidt@gmail.com>
2017-10-20 17:50:32 -0500

    added populate facts queries

--- lab3/imdb_populate_fact.sql
+++ lab3/imdb_populate_fact.sql
@@ -46,24 +46,28 @@ where average_rating >= 8.0
 group by title_type, start_year, genre;
 
 /* Part C */
-create table Title_Rating_Facts as select title_type, year, genre, appalling_titles, average_titles, outstanding_titles
-from Title_Rating_Facts_Appalling full outer join Title_Rating_Facts_Average
-full outer join Title_Rating_Facts_Outstanding;
-
-create table Title_Rating_Facts as 
-select *
-from Title_Rating_Facts_Appalling appal full outer join Title_Rating_Facts_Average avg;
+create table Title_Rating_Facts as
+select combo.title_type as title_type, combo.year as year, combo.genre as genre, appalling_titles, average_titles, outstanding_titles
+from Title_Rating_Facts_Outstanding out full outer join 
+(select avg.title_type as title_type, avg.year as year, avg.genre as genre, appalling_titles, average_titles
+from Title_Rating_Facts_Appalling appal full outer join Title_Rating_Facts_Average avg on appal.title_type = avg.title_type and appal.year = avg.year and appal.genre = avg.genre) combo on out.title_type = combo.title_type and out.year = combo.year and out.genre = combo.genre;
 
 /* Part D */
-update Title_Rating_Facts set appalling_titles = 0 where appalling_titles = null;
-update Title_Rating_Facts set average_titles = 0 where appalling_titles = null;
-update Title_Rating_Facts set outstanding_titles = 0 where appalling_titles = null;
+update Title_Rating_Facts set appalling_titles = 0 where appalling_titles is null;
+update Title_Rating_Facts set average_titles = 0 where average_titles is null;
+update Title_Rating_Facts set outstanding_titles = 0 where outstanding_titles is null;
 
 /* Part E ?*/
 delete from Title_Rating_Facts
-where title_type not distinct
+where title_type is null;
 
-alter table Title_Rating_Facts add primary key (title_type, year, genre)
+delete from Title_Rating_Facts
+where year is null;
+
+delete from Title_Rating_Facts
+where genre is null;
+
+alter table Title_Rating_Facts add primary key (title_type, year, genre);
 
 /* Part F */
 create view v_outstanding_titles_by_year_genre as 
