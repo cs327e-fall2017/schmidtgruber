91dd7f2be49f49fddef6328f615bd6e6a2a6177f
Blake Schmidt <blakecschmidt@gmail.com>
2017-10-19 19:39:55 -0500

    edited views

--- lab3/imdb_populate_fact.sql
+++ lab3/imdb_populate_fact.sql
@@ -31,7 +31,7 @@ group by title_type, start_year, genre;
 
 /* Query 2 */
 create table Title_Rating_Facts_Average as 
-select title_type, year, genre, count(*) as average_titles
+select title_type, start_year as year, genre, count(*) as average_titles
 from title_basics tb join title_genres tg on tb.title_id = tg.title_id
 join title_ratings tr on tb.title_id = tr.title_id
 where average_rating between 2.1 and 7.9
@@ -39,7 +39,7 @@ group by title_type, start_year, genre;
 
 /* Query 3 */
 create table Title_Rating_Facts_Outstanding as 
-select title_type, year, genre, count(*) as outstanding_titles
+select title_type, start_year as year, genre, count(*) as outstanding_titles
 from title_basics tb join title_genres tg on tb.title_id = tg.title_id
 join title_ratings tr on tb.title_id = tr.title_id
 where average_rating >= 8.0
@@ -47,10 +47,12 @@ group by title_type, start_year, genre;
 
 /* Part C */
 create table Title_Rating_Facts as select title_type, year, genre, appalling_titles, average_titles, outstanding_titles
-from Title_Rating_Facts_Appalling appal full outer join Title_Rating_Facts_Average avg on appal.title_type = avg.title_type
-and on appal.year = avg.year and on appal.genre = avg.genre
-full outer join Title_Rating_Facts_Outstanding out on appal.title_type = out.title_type
-and on appal.year = out.year and on appal.genre = out.genre
+from Title_Rating_Facts_Appalling full outer join Title_Rating_Facts_Average
+full outer join Title_Rating_Facts_Outstanding;
+
+create table Title_Rating_Facts as 
+select *
+from Title_Rating_Facts_Appalling appal full outer join Title_Rating_Facts_Average avg;
 
 /* Part D */
 update Title_Rating_Facts set appalling_titles = 0 where appalling_titles = null;
