f0840ec1474b80f1a28892260826ae638fa6b60e
Kyle Chance Gruber <kylegruber@wireless-10-147-160-141.public.utexas.edu>
2017-11-27 19:35:29 -0600

    added views code

---
+++ final-project/views.sql
@@ -0,0 +1,14 @@
+create view v_songs as select song_duration, count(*)
+from songs
+group by song_duration
+order by song_duration desc;
+
+create view v_singer_songs as select primary_name, person_id, count(*)
+from singer_songs ss join person_basics pb on ss.person_id = pb.person_id
+group by person_id
+order by count(person_id) desc;
+
+create view v_title_songs as select song_id, count(*)
+from title_songs
+group by song_id
+order by count(song_id) desc;
