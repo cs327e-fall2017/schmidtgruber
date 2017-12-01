778d44bad9832cff35633ef30a3a6ec310adae10
Kyle Chance Gruber <kylegruber@skrrrt.local>
2017-12-01 16:15:38 -0600

    added

--- final-project/views.sql
+++ final-project/views.sql
@@ -8,7 +8,7 @@ from singer_songs ss join person_basics pb on ss.person_id = pb.person_id
 group by primary_name, ss.person_id
 order by count(ss.person_id) desc;
 
-create view v_title_songs as select song_id, count(*)
-from title_songs
-group by song_id
-order by count(song_id) desc;
+create view v_title_songs as select ts.song_id, tb.title_type, count(*)
+from title_songs ts join title_basics tb on ts.title_id = tb.title_id
+group by tb.song_id
+order by count(*) desc;
